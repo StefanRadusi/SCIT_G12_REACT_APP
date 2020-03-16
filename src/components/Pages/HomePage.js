03fc38282a2eec236bbc72d9c5cf9a3254c06cf6
Boo1122 <you@example.com>
2020-03-16 11:49:24 +0200

    go

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -8,7 +8,7 @@ export class HomePage extends Component {
       <div className="home-page" onClick={this.increase}>
         <h1>Choose a meal that start width: </h1>
         {["A", "B", "C"].map(element => {
-          return <h3> {element} </h3>;
+          return <h3 key={element}> {element} </h3>;
         })}
       </div>
     );
